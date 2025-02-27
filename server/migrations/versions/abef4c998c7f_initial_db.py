019713f2c7f0627fc5fae125f543f62a00da7665
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-02-27 07:31:06 -0800

    initial commit

---
+++ server/migrations/versions/abef4c998c7f_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: abef4c998c7f
+Revises: 
+Create Date: 2025-02-27 07:30:48.064852
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'abef4c998c7f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
